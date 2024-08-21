c1778c9da14473b8c97a7f79d16a753483f9b0cf
Ivars Auziņš <ivars.auzins@if.lv>
2024-08-21 18:49:35 +0300

    base API for map objects

---
+++ EvCharge.Api/Controllers/PlacesController.cs
@@ -0,0 +1,27 @@
+using EV.Charge.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace EvCharge.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlacesController : ControllerBase
+    {
+        private readonly ILogger<StationsController> _logger;
+
+        public PlacesController(ILogger<StationsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(SightseeingPlace[]), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Get()
+        {
+            // read data from charging-stations.json
+            var stations = JsonConvert.DeserializeObject<List<SightseeingPlace>>(await System.IO.File.ReadAllTextAsync("Data/sightseeing.json"));
+            return Ok(stations);
+        }
+    }
+}
