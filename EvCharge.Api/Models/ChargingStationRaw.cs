e745d9d55df6ee73b40034c04171038b1c74698e
Ivars Auziņš <ivars.auzins@if.lv>
2024-08-21 21:12:18 +0300

    polyline

---
+++ EvCharge.Api/Models/ChargingStationRaw.cs
@@ -0,0 +1,11 @@
+﻿namespace EV.Charge.Models;
+
+public class ChargingStationRaw
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string GpsLocation { get; set; }
+    public int Stalls { get; set; }
+    public int FreeStalls { get; set; }
+    public string Power { get; set; }
+}
---
+++ EvCharge.Api/Models/GpsPoint.cs
@@ -0,0 +1,7 @@
+﻿namespace EvCharge.Api.Models;
+
+public class GpsPoint
+{
+      public double Latitude { get; set; }
+      public double Longitude { get; set; }
+}
